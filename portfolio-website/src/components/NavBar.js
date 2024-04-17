bd0066f6a0ce65b0f345d693a8a01eda3439bd13
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-04-17 19:10:12 +0200

    assets add

--- portfolio-website/src/components/NavBar.js
+++ portfolio-website/src/components/NavBar.js
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+
 
 export const  NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
