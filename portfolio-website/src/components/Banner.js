e7e70fd0e8dc6acde40f098d1266b2d33340ff8f
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-04-23 19:02:47 +0200

    CSS Baner

--- portfolio-website/src/components/Banner.js
+++ portfolio-website/src/components/Banner.js
@@ -48,10 +48,10 @@ export const Banner = () => {
     return(
         <section className="banner" id="home">
             <Container className="align-items-center">
-                <span className="tagline">Welcome to my portfolio</span>
-                <h1>{`Hi I'm Alexandr Kavaliou`}<span className="wrap">{text}</span></h1>
-                <p1>Lorem Ipsum</p1>
-                <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                <span className="tagline">Welcome to my portfolio </span>
+                <h1>{`Hi I'm Alexandr Kavaliou`}</h1>
+                <h1><span className="wrap">{text}</span></h1>
+                <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25} /></button>
             </Container>
         </section>
     )
