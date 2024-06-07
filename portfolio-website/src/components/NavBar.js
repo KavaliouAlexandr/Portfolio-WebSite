83437a2b1c5e8762f181be4f53d5ae6e80c3059d
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-06-07 12:18:32 +0200

    Slider

--- portfolio-website/src/components/NavBar.js
+++ portfolio-website/src/components/NavBar.js
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
@@ -45,8 +43,7 @@ const onUpdateActiveLink = (value) => {
                 </Nav>
                 <span className="navbar-text">
                     <div className="social-icon">
-                        <a href="/#"><img src={navIcon1} alt="" /></a>
-                        <a href="/#"><img src={navIcon2} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/alexandr-kavaliou-695923203/"><img src={navIcon1} alt="" /></a>
                         <a href="/#"><img src={navIcon3} alt="" /></a>
                     </div>
                     <button className="btn" onClick={() => console.log('connect')}><span>Connect</span></button>
---
+++ portfolio-website/src/components/Skills.js
@@ -0,0 +1,59 @@
+import { Col, Row, Container } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+
+export const  Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      return (
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skill-box">
+                        <h2>
+                            Skills
+                        </h2>
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Web edvelopment</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+      )
+
+}
