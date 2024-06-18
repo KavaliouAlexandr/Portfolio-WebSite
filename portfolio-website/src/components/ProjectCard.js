016e66af752e98ebd82f02542f4eeeca8a35f852
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-06-18 23:37:05 +0200

    Projects Links

--- portfolio-website/src/components/ProjectCard.js
+++ portfolio-website/src/components/ProjectCard.js
@@ -1,14 +1,16 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, linkUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
+      <a href={linkUrl} target="_blank" rel="noopener noreferrer">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
+        </a>
       </div>
     </Col>
   )
--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import projImg1 from "../assets/img/ProjImg1.png";
 import projImg2 from "../assets/img/ProjImg2.png";
 import projImg3 from "../assets/img/ProjImg3.png";
@@ -15,31 +15,37 @@ export const  Projects = () => {
           title: "Fibra.bel",
           description: " WebSite for Construction Company",
           imgUrl: projImg1,
+          linkUrl: "https://github.com/KavaliouAlexandr/Fibra.bel-PR2.git"
         },
         {
           title: "Polish Lingua",
           description: "Front-End of language learning App",
           imgUrl: projImg2,
+          linkUrl: "https://github.com/KavaliouAlexandr/Lingua.git"
         },
         {
           title: "Restauracja",
           description: "TypeScript Rest Api",
           imgUrl: projImg3,
+          linkUrl: "https://github.com/KavaliouAlexandr/RestauracjaRestAPI-PR4.git"
         },
         {
           title: "ChatBot",
           description: "ChatBotJS with Open AI API",
           imgUrl: projImg4,
+          linkUrl: "https://github.com/KavaliouAlexandr/ChatBotJS-PR3.git"
         },
         {
           title: " WebSite for Gaming Store",
           description: "Asp.Net Project with Back-End",
           imgUrl: projImg5,
+          linkUrl: "https://github.com/KavaliouAlexandr/GameStore_Exam-PR1.git"
         },
         {
           title: "Simple To-Do List",
           description: "Using TypeScript, React, Bootstrap",
           imgUrl: projImg6,
+          linkUrl: "https://github.com/KavaliouAlexandr/To-Do_PR6.git"
         },
       ];
     
